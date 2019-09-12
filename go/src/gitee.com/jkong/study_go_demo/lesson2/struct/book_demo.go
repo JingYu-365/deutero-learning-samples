11f7ca25d653294919290fb2bd520527b17e8c06
eric.zhang <eric.zhang@wingconn.com>
2019-09-12 19:06:54 +0800

    update struct

--- go/src/gitee.com/jkong/study_go_demo/lesson2/struct/book_demo.go
+++ go/src/gitee.com/jkong/study_go_demo/lesson2/struct/book_demo.go
@@ -1,60 +1,96 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "os"
+)
+
+var bookStore = make(map[string]*Book, 16)
 
 func main() {
 
-	num := 0
-	delName := ""
-	updateName := ""
-	for {
-		printmenu()
-		fmt.Scan(&num)
+    num := 0
+    for {
+        printMenu()
+        fmt.Scan(&num)
 
-		switch num {
-		case 1:
-			addBook()
-		case 2:
-			updateBook(updateName)
-		case 3:
-			listBooks()
-		case 4:
-			fmt.Scan(&delName)
-			deleteBoook(delName)
-		}
-	}
+        switch num {
+        case 1:
+            addBook()
+        case 2:
+            updateBook()
+        case 3:
+            listBooks()
+        case 4:
+            deleteBook()
+        case 5:
+            os.Exit(0)
+        }
+    }
 
 }
 
 func addBook() {
+    bookTemp := new(Book)
+    fmt.Printf("name:")
+    fmt.Scan(&bookTemp.name)
+    fmt.Printf("author:")
+    fmt.Scan(&bookTemp.author)
+    fmt.Printf("price:")
+    fmt.Scan(&bookTemp.price)
+    fmt.Printf("publish:")
+    fmt.Scan(&bookTemp.publish)
 
+    bookStore[bookTemp.name] = bookTemp
 }
 
-func updateBook(name string) {
+func updateBook() {
+    bookTemp := new(Book)
+    fmt.Printf("name:")
+    fmt.Scan(&bookTemp.name)
+    if _, ok := bookStore[bookTemp.name]; !ok {
+        fmt.Printf("book named %s not exist.", bookTemp.name)
+        return
+    }
+    fmt.Printf("author:")
+    fmt.Scan(&bookTemp.author)
+    fmt.Printf("price:")
+    fmt.Scan(&bookTemp.price)
+    fmt.Printf("publish:")
+    fmt.Scan(&bookTemp.publish)
 
+    bookStore[bookTemp.name] = bookTemp
 }
 
 func listBooks() {
-
+    for _, v := range bookStore {
+        fmt.Printf("name:%s author:%s price:%.2f publish:%t \n", v.name, v.author, v.price, v.publish)
+    }
 }
 
-func deleteBoook(i string) {
-
+func deleteBook() {
+    defer func() {
+        // todo process error
+    }()
+    var name string
+    fmt.Printf("name:")
+    fmt.Scan(&name)
+    delete(bookStore, name)
 }
 
-func printmenu()  {
-	fmt.Println("菜单：")
-	fmt.Println("1.添加书籍")
-	fmt.Println("2.更新书籍")
-	fmt.Println("3.查看书籍")
-	fmt.Println("4.删除书籍")
-	fmt.Println("请选择：")
+func printMenu() {
+    fmt.Println("菜单：")
+    fmt.Println("1.添加书籍")
+    fmt.Println("2.更新书籍")
+    fmt.Println("3.查看书籍")
+    fmt.Println("4.删除书籍")
+    fmt.Println("5.退出")
+    fmt.Println("请选择：")
 }
-
 
 type Book struct {
-	name string
-	author string
-	price float32
-	publish bool
+    name    string
+    author  string
+    price   float32
+    publish bool
 }
