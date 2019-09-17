34712d73ebd6274d55bdca9656d3e69df721d6c9
jdkong <zhangduanhe@126.com>
2019-09-18 07:59:14 +0800

    add other log level method

--- go/src/gitee.com/jkong/study_go_demo/lesson05/mylogger/file.go
+++ go/src/gitee.com/jkong/study_go_demo/lesson05/mylogger/file.go
@@ -11,6 +11,7 @@ import (
 
 // FileLogger 文件日志结构体
 type FileLogger struct {
+	level Level
 	fileName string
 	filePath string
 	file     *os.File
@@ -50,8 +51,13 @@ func (f *FileLogger) initFile() {
 	f.errFile = errorFileObj
 }
 
-// 方法
+// Debug 方法
 func (f *FileLogger) Debug(format string, args ...interface{}) {
+	// 如果当前的日志等级大于Debug则不打印
+	if f.level > DebugLevel {
+		return
+	}
+
 	//f.file.Write()
 	// F：生成一个文件
 	msg := fmt.Sprintf(format, args...) // 得到用户记录的日志
@@ -75,4 +81,72 @@ func (f *FileLogger) Debug(format string, args ...interface{}) {
 
 	// S：生成一个字符串
 	//fmt.Sprintf()
+}
+
+// Info 方法
+func (f *FileLogger) Info(format string, args ...interface{}) {
+	// 得到用户记录的日志
+	msg := fmt.Sprintf(format, args...)
+
+	// 日志格式：[时间][文件：行号][函数名][日志级别] 日志信息
+	nowStr := time.Now().Format("2006-01-02 15:04:05.000")
+	fileName, funcName, line := getCallerInfo(2)
+	logMsg := fmt.Sprintf("[%s][%s:%d][%s][%s] %s \n", nowStr, fileName, line, funcName, "Info", msg)
+
+	// 利用fmt包将msg字符串写到f.file文件中
+	_, err := fmt.Fprintf(f.file, logMsg)
+	if err != nil {
+		panic(fmt.Errorf("debug log msg error: %v", err))
+	}
+}
+
+// Warn 方法
+func (f *FileLogger) Warn(format string, args ...interface{}) {
+	// 得到用户记录的日志
+	msg := fmt.Sprintf(format, args...)
+
+	// 日志格式：[时间][文件：行号][函数名][日志级别] 日志信息
+	nowStr := time.Now().Format("2006-01-02 15:04:05.000")
+	fileName, funcName, line := getCallerInfo(2)
+	logMsg := fmt.Sprintf("[%s][%s:%d][%s][%s] %s \n", nowStr, fileName, line, funcName, "Warn", msg)
+
+	// 利用fmt包将msg字符串写到f.file文件中
+	_, err := fmt.Fprintf(f.file, logMsg)
+	if err != nil {
+		panic(fmt.Errorf("debug log msg error: %v", err))
+	}
+}
+
+// Error 方法
+func (f *FileLogger) Error(format string, args ...interface{}) {
+	// 得到用户记录的日志
+	msg := fmt.Sprintf(format, args...)
+
+	// 日志格式：[时间][文件：行号][函数名][日志级别] 日志信息
+	nowStr := time.Now().Format("2006-01-02 15:04:05.000")
+	fileName, funcName, line := getCallerInfo(2)
+	logMsg := fmt.Sprintf("[%s][%s:%d][%s][%s] %s \n", nowStr, fileName, line, funcName, "Error", msg)
+
+	// 利用fmt包将msg字符串写到f.file文件中
+	_, err := fmt.Fprintf(f.file, logMsg)
+	if err != nil {
+		panic(fmt.Errorf("debug log msg error: %v", err))
+	}
+}
+
+// Fatal 方法
+func (f *FileLogger) Fatal(format string, args ...interface{}) {
+	// 得到用户记录的日志
+	msg := fmt.Sprintf(format, args...)
+
+	// 日志格式：[时间][文件：行号][函数名][日志级别] 日志信息
+	nowStr := time.Now().Format("2006-01-02 15:04:05.000")
+	fileName, funcName, line := getCallerInfo(2)
+	logMsg := fmt.Sprintf("[%s][%s:%d][%s][%s] %s \n", nowStr, fileName, line, funcName, "Fatal", msg)
+
+	// 利用fmt包将msg字符串写到f.file文件中
+	_, err := fmt.Fprintf(f.file, logMsg)
+	if err != nil {
+		panic(fmt.Errorf("debug log msg error: %v", err))
+	}
 }
