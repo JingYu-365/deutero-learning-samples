7dd8a936132473d65932e025f392788e58185c00
JKongZhang <zhangduanhe@126.com>
2020-06-09 07:45:52 +0800

    add sql file

---
+++ java/spring-cloud-samples/spring_cloud.sql
@@ -0,0 +1,30 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : LOCAL
+ Source Server Type    : MySQL
+ Source Server Version : 50724
+ Source Host           : localhost:3306
+ Source Schema         : spring_cloud
+
+ Target Server Type    : MySQL
+ Target Server Version : 50724
+ File Encoding         : 65001
+
+ Date: 09/06/2020 07:44:56
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for payment
+-- ----------------------------
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE `payment` (
+  ` id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  ` serial` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (` id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
