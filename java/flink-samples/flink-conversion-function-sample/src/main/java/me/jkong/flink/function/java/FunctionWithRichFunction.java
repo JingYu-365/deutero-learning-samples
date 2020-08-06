0b3733d0672560c424b6128609e188e8641ee97f
JKongZhang <zhangduanhe@126.com>
2020-08-07 07:26:02 +0800

    flink - conversion function

---
+++ java/flink-samples/flink-conversion-function-sample/src/main/java/me/jkong/flink/function/java/FunctionWithRichFunction.java
@@ -0,0 +1,11 @@
+package me.jkong.flink.function.java;
+
+/**
+ * 使用RichFunction实现类处理数据转换
+ *
+ * @author JKong
+ * @version v0.0.1
+ * @date 2020/8/7 06:58.
+ */
+public class FunctionWithRichFunction {
+}
