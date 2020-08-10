2cd1ba69989e74a89dd8354bf3f80573cfbe6c30
JKongZhang <zhangduanhe@126.com>
2020-08-10 16:26:54 +0800

    flink - init flink source and sink project

---
+++ java/flink-samples/flink-data-sink-sample/src/main/java/me/jkong/flink/sink/java/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * TODO
+ *
+ * @author JKong
+ * @version v0.0.1
+ * @date 2020/8/10 15:29.
+ */
+package me.jkong.flink.sink.java;
+
+
+
---
+++ java/flink-samples/flink-data-source-sample/src/main/java/me/jkong/flink/source/java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * TODO
+ *
+ * @author JKong
+ * @version v0.0.1
+ * @date 2020/8/10 16:24.
+ */
+package me.jkong.flink.source.java;
