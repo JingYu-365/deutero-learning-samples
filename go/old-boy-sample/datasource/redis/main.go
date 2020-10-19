099cb6ee2dcbca8e07136b9408f960c44601a831
JKongZhang <zhangduanhe@126.com>
2020-10-19 21:27:19 +0800

    go - redis zset

--- go/old-boy-sample/datasource/redis/main.go
+++ go/old-boy-sample/datasource/redis/main.go
@@ -23,5 +23,33 @@ func init() {
 }
 
 func main() {
+	zsetOperation()
+}
 
+func zsetOperation() {
+	zkey := "rank"
+	items := []redis.Z{
+		redis.Z{Score: 96, Member: "Java"},
+		redis.Z{Score: 93, Member: "Javascript"},
+		redis.Z{Score: 97, Member: "Python"},
+		redis.Z{Score: 95, Member: "CSS"},
+		redis.Z{Score: 99, Member: "Golang"},
+	}
+
+	redisClient.ZAdd(zkey, items...)
+
+	newScore, err := redisClient.ZIncrBy(zkey, 10.0, "Golang").Result()
+	if err != nil {
+		fmt.Printf("zIncrBy failed, err: %v \n", err)
+	}
+	fmt.Printf("new score: %f \n", newScore)
+
+	// 获取分数最高的三个
+	ret, err := redisClient.ZRevRangeWithScores(zkey, 0, 2).Result()
+	if err != nil {
+		fmt.Printf("get best 3 failed, err: %v \n", err)
+	}
+	for _, z := range ret {
+		fmt.Println(z.Member, z.Score)
+	}
 }
